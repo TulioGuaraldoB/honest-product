2b7aeeee5731a26063853363d7b7c1ddfd22b450
Tulio Guaraldo <tulio.brugin@q2pay.com.br>
2022-10-25 17:41:48 -0300

    add id tag

--- Web/Models/Product.cs
+++ Web/Models/Product.cs
@@ -1,9 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HonestProduct.Web.Models;
 
 public class Product
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public string? Name { get; set; }
